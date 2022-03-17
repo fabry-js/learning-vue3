14e3133324e4bc39ce96b5028b5155f4d280c141
fabry-js <vivaldifabrizio10@gmail.com>
2022-03-17 11:37:33 +0100

    Name transform

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,7 +8,28 @@ const data: Ref<string[]> = ref([])
 
 onMounted(async() => {
   await axios.get(`${apiUrl}/characters`)
-    .then((res) => data.value = res.data)
+    .then((res) => {
+      const originData: string[] = res.data
+      const finalArr: string[] = []
+
+      originData.forEach((charName) => {
+        let _uppedName = charName.replace(charName[0], charName[0].toUpperCase())
+        let finalName = ""
+        if(_uppedName.includes("-") === true){
+          const _dashNameArray = _uppedName.replace("-", " ").split(" ")
+          const _dashNameFirstUppedPortion = _dashNameArray[0]
+          const _dashNameLowerPortion = _dashNameArray[1]
+          const _uppedLowerPortion = _dashNameLowerPortion.replace(_dashNameLowerPortion[0], _dashNameLowerPortion[0].toUpperCase())
+
+          finalName = `${_dashNameFirstUppedPortion} ${_uppedLowerPortion}`
+        } else {
+          finalName = _uppedName
+        }
+        finalArr.push(finalName)
+      })
+
+      data.value = finalArr
+    })
 })
 
 </script>
