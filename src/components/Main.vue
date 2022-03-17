d2df4a09393867da17b0542e749f98ac4b36704e
fabry-js <vivaldifabrizio10@gmail.com>
2022-03-17 10:29:43 +0100

    Basic API

---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { onMounted, Ref, ref } from "vue";
+import axios from "axios";
+
+import { apiUrl } from "../constants";
+
+const data: Ref<string[]> = ref([])
+
+onMounted(async() => {
+  await axios.get(`${apiUrl}/characters`)
+    .then((res) => data.value = res.data)
+})
+
+</script>
+
+<template>
+  <div v-for="pg in data">
+    {{ pg }}
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
